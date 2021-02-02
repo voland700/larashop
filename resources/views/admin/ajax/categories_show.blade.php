5349120762c8700d6fca834a692f4cdc8fc0a6f9
voland700 <voland700@yandex.ru>
2021-02-02 19:16:32 +0300

    add template for ajax discount

--- resources/views/admin/ajax/categories_show.blade.php
+++ resources/views/admin/ajax/categories_show.blade.php
@@ -10,21 +10,19 @@
                 </tr>
                 </thead>
                 <tbody class="d_table">
-
-
-
-
-
+                @php
+                    $traverse = function ($categories, $prefix = '-&ensp;') use (&$traverse) {
+                            foreach ($categories as $category) {
+                                $td =  '<tr><td><span class="d_btn btn btn-block btn-default" data-id="'.$category->id.'" data-name="'.$category->name.'"><i class="far fa-check-square fa-sm"></i></span></td><td>';
+                                $td .= PHP_EOL.$prefix.' '.$category->name.'</td><td>'.$category->id.'</td></tr>';
+                                echo $td;                             
+                                $traverse($category->children, $prefix.'-&ensp;');
+                            }
+                    };
+                    $traverse($categories);
+                @endphp
                 </tbody>
             </table>
-
-            <ul>
-                @foreach($categories as $category)
-                    <li>{{$category->depth}} - {{$category->name}}</li>
-                @endforeach
-            </ul>
-
-
 
         </div>
     </div>
